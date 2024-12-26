648025ac33a03bb88ab3cde0369501611fad7522
Jack <39860321+jackstirzaker@users.noreply.github.com>
2024-12-26 21:50:02 +0000

    Add new content!

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), alpinejs()],
 
   site: 'https://www.jackstirzaker.com',
 });
