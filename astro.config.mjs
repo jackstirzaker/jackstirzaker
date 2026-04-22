d175bd2f6e21d7d7907213e927e9f25db7a64092
Jack Stirzaker <39860321+jackstirzaker@users.noreply.github.com>
2026-04-22 15:38:44 +0100

    Upgrade various things idk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
-
+import tailwindcss from "@tailwindcss/vite";
 import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()],
+  integrations: [alpinejs()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 
   site: 'https://www.jackstirzaker.com',
 });
